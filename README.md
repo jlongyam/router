682a47c40ed558a51e8f63848066c30239aa31bc
jlongyam <jhonliongbox@gmail.com>
2023-10-26 03:29:50 +0700

    Fix bug after long time

--- README.md
+++ README.md
@@ -1,57 +1,30 @@
-# Router
-
-[github.com/jlongyam/router](https://github.com/jlongyam/router)
-
-## What is this ?
-
-Simple hash based **router** via `location.hash` change,
-
-no  `History.pushState()`
+# [router](https://github.com/jlongyam/router)
 
 ## Usage
 
 ### HTML
 
 ```html
-<script src="./Router.js"></script>
 <ul>
   <li><a href="#/">Root</a></li>
   <li><a href="#/Home">Home</a></li>
+  <li><a href="#/Page/About">Home</a></li>
 </ul>
-<ul>
-  <li><a href="./simple.html">Reset</a></li>
-</ul>
-<pre id="output"></pre>
 ```
 
-### Javascript
+### JS
 
 ```javascript
 var router = new Router({
-  '#/': ()=> output.textContent = 'Index',
-  '#/Home/': ()=> output.textContent = 'Welcome'
+  '#/': ()=> ... ,
+  '#/Home/': ()=> ... ,
+  '#/Page/:any': (any)=> ... any ... 
 })
 ```
 
-[Demo](https://jlongyam.github.io/router/test/index.html)
-
 ## Note
 
-- Writing map **without** argument MUST ending with `'/'` slash
-- Writing map **with** argument **NO** slash `'/'` at end
-- There is no **404** page
+- Writing map __without argument:__ _Must_ ending with `'/'` slash.
+- Writing map __with argument:__ _NO slash_ `'/'` at end.
 
-## CDN
-
-- [Router.min.js](https://rawcdn.githack.com/jlongyam/router/689a55d62e70bb27e38eb10c8f96dcad5e132bb8/dist/Router.min.js)
-- [Router.m.min.js](https://rawcdn.githack.com/jlongyam/router/689a55d62e70bb27e38eb10c8f96dcad5e132bb8/dist/Router.m.min.js)
-- [Router.fallback.min.js](https://rawcdn.githack.com/jlongyam/router/689a55d62e70bb27e38eb10c8f96dcad5e132bb8/dist/Router.fallback.min.js)
-
-## Contribute
-
-- fork
-- pull
-- issue any bug
-- fix
-
-License **HELL**
+[Changelog](changelog.md)
--- src/Router.js
+++ src/Router.js
@@ -1,73 +1,103 @@
 class Router {
-  constructor( map = {} ) {
+  constructor(map = {}) {
     this.map = map
-    window.addEventListener( 'hashchange', ()=> { return this._hash })
-    if( window.location.hash.length > 0 ) return this._hash
-  }
-  _exec( hash, ahash, amap ) {
-    var maps = Object.keys( this.map )
-    for( var i = 0; i < maps.length; i++ ) {
-      if( maps[i] === hash ) {
-        if( typeof ahash != 'undefined' ) {
-          var args = ':' + amap.join('/:')
-          this.map[ hash + args ].apply( null, ahash )
-        }
-        else this.map[hash].apply()
-      }
+    window.addEventListener('hashchange', async () => {
+      this._listen
+    })
+    if (window.location.hash.length > 0) {
+      this._listen
     }
   }
-  _sort( match, hash ) {
-    for( var i = 0; i < match.length; i++ ) {
-      var map = match[i]['map']
-      var tail = hash.substring( map.length )
-      var arg = match[i]['variable']
-      if( tail.length === 0 ) {
-        if( arg.length === 0 ) this._exec( map )
-      }
+  _exec(hash, vars, args) {
+    // evaluate map, then comparison
+    let maps = Object.keys(this.map)
+    let key = false, cb = false;
+    // find key and cb by URL hash
+    // ========================================================
+    // let report = '' // DON't delete this comments
+    // report += '\n'
+    // report += 'location.hash: ' + location.hash + '\n'
+    // report += '====================================\n'
+    // report += 'hash: ' + hash + '\n'
+    // report += 'vars: ' + vars + '\n'
+    // report += 'args: ' + args + '\n'
+    // report += '====================================\n'
+    // report += 'select: ' + 'this.map[' + hash + args + ']' + '\n'
+    // report += 'cb: (' + args + ')\n'
+    // report += '====================================\n'
+    // console.log(report)
+    // ========================================================
+    for (let i = 0; i < maps.length; i++) {
+      try {
+        if ('' + args === 'undefined') this.map[hash].call()
       else {
-        if( arg.length > 0 ) {
-          var hash_no_tail = tail.substring( 0, tail.length - 1)
-          var a_hash = hash_no_tail.split('/')
-          if( a_hash.length === arg.length ) this._exec( map, a_hash, arg )
+        let _args = ':' + args.join('/:')
+        this.map[hash + _args].apply(null, vars)
+      }
+      }
+      catch(e) {
+        e.message = '\nError:'
+        e.message += '\nPlease check URL writing'
+        e.message += '\nThis message from "_exec"'
+        console.log(e.message )
+      }
+      
+    }
+  }
+  _sort(match, hash) {
+    for (let i = 0; i < match.length; i++) {
+      let map = match[i]['map']
+      let tail = hash.substring(map.length)
+      let arg = match[i]['variable']
+      if (tail.length === 0) {
+        if (arg.length === 0) this._exec(map)
+      }
+      else { // tail.length > 0
+        if (arg.length > 0) {
+          let hash_no_tail = tail.substring(0, tail.length - 1)
+          let a_hash = hash_no_tail.split('/')
+          if (a_hash.length === arg.length) this._exec(map, a_hash, arg)
         }
       }
     }
   }
-  get _map() {
-    const maps = Object.keys( this.map )
+  get _filter() {
+    const maps = Object.keys(this.map)
     let a = []
-    for( var i = 0; i < maps.length; i++ ) {
-      var hashs = maps[i].split(':')
-      var base = hashs[0]
-      var normalize = base.substring( base.length -1 ) != '/' ? base + '/' : base
-      var vars = []
-      for( var u = 0; u < hashs.length; u++ ) {
-        if( hashs[u] === base ) continue;
-        vars.push( hashs[u].replace('/', '') )
+    for (let i = 0; i < maps.length; i++) {
+      let hashs = maps[i].split(':')
+      let base = hashs[0]
+      let normalize = base.substring(base.length - 1) != '/' ? base + '/' : base
+      let vars = []
+      for (let u = 0; u < hashs.length; u++) {
+        if (hashs[u] === base) continue
+        let a_hash_u = hashs[u].split('/')
+        if (a_hash_u.includes('')) a_hash_u.pop()
+        vars.push(a_hash_u)
       }
       a.push({
         hash: maps[i],
         base: normalize,
         variable: vars,
-      })      
+      })
     }
     return a
   }
-  get _hash() {
-    const hash = decodeURI( window.location.hash )
-    var tail = hash.substring( hash.length, hash.length - 1 )
-    var normalize = tail === '/' ? hash :  hash + '/'
+  get _listen() {
+    const hash = decodeURI(window.location.hash)
+    let tail = hash.substring(hash.length, hash.length - 1)
+    let normalize = tail === '/' ? hash : hash + '/'
     let a = []
-    for( var i = 0; i < this._map.length; i++ ) {
-      var base = this._map[i]['base']
-      if( normalize.indexOf( base ) != -1 ) {
+    for (let i = 0; i < this._filter.length; i++) {
+      let base = this._filter[i]['base']
+      if (normalize.indexOf(base) != -1) {
         a.push({
           map: base,
-          variable: this._map[i]['variable']
+          variable: this._filter[i]['variable']
         })
       }
     }
-    this._sort( a, normalize )
+    this._sort(a, normalize)
     return {
       hash: hash,
       normalize: normalize,
